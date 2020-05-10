6d16df953143d226f43a4e227538b9f28e2b5f5d
Casper Dik <casperdik3@gmail.com>
2020-05-10 17:20:33 +0200

    quantecon

--- chapter_15/practise matplotlib/quantecon4.py
+++ chapter_15/practise matplotlib/quantecon4.py
@@ -9,12 +9,17 @@ def f(x, y):
     return np.cos(x ** 2 + y ** 2) / (1 + x ** 2 + y ** 2)
 
 
+def f1(x, y):
+    return (np.sin(5 * x) * np.cos(5 * y)) / 5
+
+
 xgrid = np.linspace(-3, 3, 50)
 ygrid = xgrid
 x, y = np.meshgrid(xgrid, ygrid)
 
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, f(x, y), rstride=2, cstride=2, cmap=cm.jet, alpha=0.7, linewidth=0.25)
-ax.set_zlim(-0.5, 1.0)
+ax.plot_surface(x, y, f(x, y), rstride=1, cstride=1, cmap=cm.jet, alpha=0.7, linewidth=0.25)
+ax.set_zlim(-0.5, 1)
+
 plt.show()
