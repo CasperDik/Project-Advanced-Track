55106a98aeef145b87ec14a7c2fee1064f08c956
Casper Dik <casperdik3@gmail.com>
2020-06-11 16:55:17 +0200

    assessment

--- final_assessment/CleanUpLauncher.py
+++ final_assessment/CleanUpLauncher.py
@@ -3,7 +3,6 @@ from tkinter import *
 from CleanUpGui import CleanUpGui
 
 root = Tk()
-# root.geometry("300x300")
 cleanup = CleanUpGui(root)
 # messy folder should be a sub-folder in your project.
 cleanup.select_folder()
